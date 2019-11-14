4c5292dd271022da36e235c94bd43deb7544d9a4
Balaji197 <57744320+Balaji197@users.noreply.github.com>
2019-11-14 12:08:21 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+public class Main{
+public static void main(String[]args){
+	
+	     int multiply1 = 1000 * 5; // In multiplication the operators used is *
+	     int multiply2 = multiply1 * 98;
+		 int multiply3 = multiply2 * multiply2;
+           System.out.println(multiply3);
+	
+	
+	
+	
+	
+         }
+
+
+    }
